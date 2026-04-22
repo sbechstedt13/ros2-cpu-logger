a572d86fbbef5d935a7b4c13ebb0f4d45c8674db
Simon Bechstedt <S.Bechstedt@web.de>
2026-04-22 11:30:11 +0200

    rm user input for log-file as it is currently hardcoded into ROS2 node

--- monitor_cpu_load.sh
+++ monitor_cpu_load.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-while getopts c:f: flag
+# LOG_FILE currently hardcoded in cpu_util_publisher.cpp
+# can be found in ROS2 Workspace cpu_load
+# TODO: Take user input for Logfile
+while getopts c: flag
 do
     case "${flag}" in
         c) CONTAINER_NAME=${OPTARG};;
-        f) LOG_FILE=${OPTARG};;
+        #f) LOG_FILE=${OPTARG};;
     esac
 done
 
-docker exec -it $CONTAINER_NAME /bin/bash -c "tail -f /logs/$LOG_FILE"
+docker exec -it $CONTAINER_NAME /bin/bash -c "tail -f /logs/cpu_load.log"
